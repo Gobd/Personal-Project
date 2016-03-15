76efa346ad89f0c87677126a5870b9ecfcf6efc2
Brian Kemper <bkemper@gmail.com>
2016-03-15 08:02:41 -0600

    stupid...

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  'TOKEN_SECRET' : 'asdioasfasdffdgdfgdfgdfgdfgdfwwfasf',
+  'TOKEN_SECRET' : '10MOzFy9u1b1qGy01oF0j4gUMqLRAq8mdNeSjdvBUDVVK11js3qXYFOGkvVbIYpj35zXLlFPIRT6qmmTrvTiiSfaEUAmKc85BnU3KKEATWLpjUgGhK8Wybc7MH5e5h5o',
     'facebookAuth' : {
         'clientID'      : '1670205403245071',
         'clientSecret'  : '07ec98a15ecc5853a36fc8287c3ea41c',
